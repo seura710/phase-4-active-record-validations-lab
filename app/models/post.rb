884e8dfb89c826e7c2506b3b99c45b294a830cbd
Harris Kim <seura710@gmail.com>
2021-12-14 10:31:03 -0500

    completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, { :length => {:minimum => 250}}
+    validates :summary, { :length => {:maximum => 250}}
+    validate :category_type
+
+    def category_type
+        if category !== "Non-Fiction" || "Fiction"
+            errors.add(:category, "Error!")
+        end
+    end
 end
